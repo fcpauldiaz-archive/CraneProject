6cd36ec88395da7c9550c58968e0b1756a39ab29
Pablo Díaz <pablo5_diaz@hotmail.com>
2017-04-25 13:32:30 -0600

    WIP: collisions and move container with boat

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,17 +16,20 @@ window.addEventListener('DOMContentLoaded', function() {
     var shearZ = 0;
     var actualElement = false; //false is for boat, true for crane
     var meshesColliderList = [];
+    var plan2;
+    var matPlan;
+    var currentHoldingContainer;
+    var holding = false;
+    var containersWithShip = [];
 
     engine.enableOfflineSupport = false;
 
     var createScene = function() {
         var scene = new BABYLON.Scene(engine);
         scene.clearColor = new BABYLON.Color3.White();
-        scene.enablePhysics(new BABYLON.Vector3(0, -10, 0), new BABYLON.OimoJSPlugin());
-        var gravity = new BABYLON.Vector3(0, -9.81, 0);
-        scene.enablePhysics(gravity, new BABYLON.OimoJSPlugin());
-        scene.gravity = gravity;
+        scene.gravity = new BABYLON.Vector3(0, -9.81, 0);
         scene.collisionsEnabled = true;
+       
 
         var camera = new BABYLON.ArcRotateCamera(
             "arcCam",
@@ -37,8 +40,10 @@ window.addEventListener('DOMContentLoaded', function() {
             scene
         );
         camera.attachControl(canvas, true);
+        camera.applyGravity = true; 
+        camera.ellipsoid = new BABYLON.Vector3(1, 1, 1);
         camera.checkCollisions = true;
-        camera.applyGravity = true;
+        camera.collisionRadius = new BABYLON.Vector3(0.5, 0.5, 0.5)
 
         var light = new BABYLON.HemisphericLight(
             "HemisphericLight",
@@ -54,41 +59,40 @@ window.addEventListener('DOMContentLoaded', function() {
             function(newMeshes) {
                 newMeshes.forEach(
                     function(mesh) {
-                        console.log(mesh);
-                        mesh.showBoundingBox = true;
-                        mesh._checkCollisions = true;
-                        mesh.checkCollisions = function(en) {
-                            console.log('true');
-                        }
-                        console.log(mesh);
-                        console.log('d')
                         elements.push(mesh);
                     }
                 );
             }
         );
 
-        elements.forEach(function(element) {
-            element.checkCollisions = true;
-            element.showBoundingBox = true;
-            element.setPhysicsState(BABYLON.PhysicsEngine.BoxImpostor, { mass: 0, 
-                                                friction: 0.5, restitution: 0.7 });
-        });
 
-        scene.registerBeforeRender(function() {});
+        scene.meshes.forEach(function(element) {
+            element.checkCollisions = true;
+            element.boundingBox = true;
+        });
+         
+       
+        scene.registerBeforeRender(function() {
+            elements.forEach(function(element) {
+                element.checkCollisions = true;
+                element.boundingBox = true;
+                //element.moveWithCollisions();
+            });
+        });
 
         // Water
         var waterMesh = BABYLON.Mesh.CreateGround("waterMesh", 512, 512, 32, scene, false);
         
         var water = new BABYLON.WaterMaterial("water", scene);
         water.bumpTexture = new BABYLON.Texture("model/water/waterbump.png", scene);
-        
+
         // Ground
         var groundMaterial = new BABYLON.StandardMaterial("groundMaterial", scene);
         groundMaterial.diffuseTexture = new BABYLON.Texture("model/ground.jpg", scene);
         groundMaterial.diffuseTexture.uScale = groundMaterial.diffuseTexture.vScale = 4;
         
         var ground = BABYLON.Mesh.CreateGround("ground", 512, 512, 32, scene, false);
+        ground.checkCollisions = true;
         ground.position.y = -11;
         ground.material = groundMaterial;
 
@@ -107,6 +111,9 @@ window.addEventListener('DOMContentLoaded', function() {
         waterMesh.material = water;
         waterMesh.position.y = -10;
 
+
+       
+
         return scene;
     }
 
@@ -122,50 +129,165 @@ window.addEventListener('DOMContentLoaded', function() {
         //W
         if (evt.keyCode == 87) {
             if (actualElement === true) {
-                 rotateElements(elements.filter(getCrane)[0], BABYLON.Axis.Y, -Math.PI / 12);
+                rotateElements(elements.filter(getClaw)[0], BABYLON.Axis.Y, -Math.PI / 12);
             }
             else {
                 var transVector = new BABYLON.Vector3(0, 0, -1);
-                translateElements(elements[0], transVector);
+                var transVectorContainer = new BABYLON.Vector3(1, 0, 0);
+                translateElements(elements.filter(getBoat)[0], transVector);
+                for (var i = 0; i < containersWithShip.length; i++) {
+                    var container = containersWithShip[i];
+                    translateElements(container, transVectorContainer);
+                }
             }
         }
 
         //S
         if (evt.keyCode == 83) {
             if (actualElement === true) {
-                rotateElements(elements.filter(getCrane)[0], BABYLON.Axis.Y, Math.PI / 12);
+                rotateElements(elements.filter(getClaw)[0], BABYLON.Axis.Y, Math.PI / 12);
+                
             }
             else {
                 var transVector = new BABYLON.Vector3(0, 0, 1);
-                console.log(elements);
                 translateElements(elements.filter(getBoat)[0], transVector);
+                var transVectorContainer = new BABYLON.Vector3(-1, 0, 0);
+                translateElements(elements.filter(getBoat)[0], transVector);
+                for (var i = 0; i < containersWithShip.length; i++) {
+                    var container = containersWithShip[i];
+                    translateElements(container, transVectorContainer);
+                }
+
             }
         }
 
         //A
         if (evt.keyCode == 65) {
             if (actualElement === true) {
-                rotateElements(elements.filter(getClaw)[0], BABYLON.Axis.Y, Math.PI / 12);
+                var crane = elements.filter(getCrane)[0];
+                rotateElements(crane, BABYLON.Axis.Y, Math.PI / 12);
+                if (holding) {
+                    var claw = elements.filter(getClaw)[0];
+                    currentHoldingContainer.position.x = claw.getAbsolutePosition().x;
+                    currentHoldingContainer.position.y = claw.getAbsolutePosition().y;
+                    currentHoldingContainer.position.z = claw.getAbsolutePosition().z;
+                }
             } else {
                 rotateElements(elements.filter(getBoat)[0], BABYLON.Axis.Y, -Math.PI / 16);
+                for (var i = 0; i < containersWithShip.length; i++) {
+                    var container = containersWithShip[i];
+                    rotateElements(container, BABYLON.Axis.Y, -Math.PI / 16);
+                }
             }
         }
 
         //D
         if (evt.keyCode == 68) {
             if (actualElement === true) {
-                rotateElements(elements.filter(getClaw)[0], BABYLON.Axis.Y, -Math.PI / 12);
+                var crane = elements.filter(getCrane)[0]
+                rotateElements(crane, BABYLON.Axis.Y, -Math.PI / 12);
+                if (holding) {
+                    var claw = elements.filter(getClaw)[0];
+                    currentHoldingContainer.position.x = claw.getAbsolutePosition().x;
+                    currentHoldingContainer.position.y = claw.getAbsolutePosition().y;
+                    currentHoldingContainer.position.z = claw.getAbsolutePosition().z;
+                }
             }
             else {
                 rotateElements(elements.filter(getBoat)[0], BABYLON.Axis.Y, Math.PI / 16);
+                for (var i = 0; i < containersWithShip.length; i++) {
+                    var container = containersWithShip[i];
+                    rotateElements(container, BABYLON.Axis.Y, Math.PI / 16);
+                }
             }
         }
-        //space bar
-        if (evt.keyCode === 32) {
-            console.log(elements);
-            scaleElements(elements.filter(getCrane)[0], dimensions[1], 1.1);
-        }
+        // x
+        if (evt.keyCode === 88) {
+            
+            var claw = elements.filter(getClaw)[0];
+            var rope = elements.filter(getRope)[0];
+            var boat = elements.filter(getBoat)[0];
+            
+            var clawPosition = claw.getAbsolutePosition();
+            var boatPosition = boat.getAbsolutePosition();
+          /*  console.log('boat')
+            console.log(boatPosition.z)
+            console.log(boatPosition.x)
+            console.log('claw')
+            console.log(clawPosition.z)
+            console.log(clawPosition.x)*/
+            var center = clawPosition.x
+            if ((Math.abs(clawPosition.y) -3) < Math.abs(boatPosition.y)
+                && center > (boatPosition.x - 14.0) && center < (boatPosition.x + 20.77)
+                ) {
+                //drop on choque
+                console.log('choca');
+            } else {
+                var transVector = new BABYLON.Vector3(0, -0.5, 0);
+                var transVector2 = new BABYLON.Vector3(0, -1, 0);
+                translateElements(claw,transVector2);
+                scaleElements(rope, dimensions[1], 1.1);
+                translateElements(rope, transVector);
+            }
+            if (holding) {
+                var claw = elements.filter(getClaw)[0];
+                currentHoldingContainer.position.x = claw.getAbsolutePosition().x;
+                currentHoldingContainer.position.y = claw.getAbsolutePosition().y;
+                currentHoldingContainer.position.z = claw.getAbsolutePosition().z;
+            }
+         
 
+
+        }
+        // z
+        if (evt.keyCode === 90) {
+            var claw = elements.filter(getClaw)[0];
+            var rope = elements.filter(getRope)[0];
+            var transVector = new BABYLON.Vector3(0, 0.6, 0);
+            var transVector2 = new BABYLON.Vector3(0, 1, 0);
+            translateElements(claw,transVector2);
+            scaleElements(rope, dimensions[1], 0.9);
+            translateElements(rope, transVector);
+            if (holding) {
+                var claw = elements.filter(getClaw)[0];
+                currentHoldingContainer.position.x = claw.getAbsolutePosition().x;
+                currentHoldingContainer.position.y = claw.getAbsolutePosition().y;
+                currentHoldingContainer.position.z = claw.getAbsolutePosition().z;
+            }
+        }
+        // spacebar
+        if (evt.keyCode === 32) {
+            //dropoff command
+            if (holding) {
+                if (!containersWithShip.includes(currentHoldingContainer)) {
+                    containersWithShip.push(currentHoldingContainer);
+                }
+            }
+
+            //hold one container
+            //check for containers
+            var containers = elements.filter(getContainers);
+            var clawPosition = elements.filter(getClaw)[0].getAbsolutePosition();
+            var size = containers[0].getBoundingInfo().boundingBox.extendSize;
+            var closer_container = undefined;
+            var currentDiff = -10000;
+            for (var i = 0 ; i < containers.length; i++) {
+                var c1 = containers[i];
+                var posC1 = c1.getAbsolutePosition();
+                if (Math.abs(posC1.y) - Math.abs(clawPosition.y) > currentDiff) {
+                    currentDiff = Math.abs(posC1.y) - Math.abs(clawPosition.y);
+                    closer_container = c1;
+                }
+                console.log('verify')
+                console.log(posC1.y)
+                console.log(clawPosition.y)
+                console.log(posC1.y - clawPosition.y)
+                console.log(c1.id);
+            }
+            console.log(closer_container.id);
+            currentHoldingContainer = closer_container
+            holding = !holding;
+        }
     
 
         //help
@@ -184,7 +306,7 @@ window.addEventListener('DOMContentLoaded', function() {
             help = !help;
         }
 
-        console.log(evt.keyCode);
+        //console.log(evt.keyCode);
     };
 
     var wireFrame = function(elements) {
@@ -245,9 +367,19 @@ window.addEventListener('DOMContentLoaded', function() {
     function getCrane(item) {
         return item.id === "Crane";
     }
+    function getClaw(item) {
+        return item.id === "Claw";
+    }
 
     function getBoat(item) {
         return item.id === "Shiping_";
+    }
+    function getRope(item) {
+        return item.id === "Rope";
+    }
+
+    function getContainers(item) {
+        return item.id.includes("Container");
     }
 
 
